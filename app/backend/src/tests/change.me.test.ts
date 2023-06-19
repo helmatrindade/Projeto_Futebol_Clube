48d7fd99fa22efe50bfb9b3f729a31afd50ea9a1
helma trindade <helmacaires28@gmail.com>
2023-06-19 17:41:07 -0300

    Feito Requisito 19

--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -6,39 +6,11 @@ import chaiHttp = require('chai-http');
 import { app } from '../app';
 import Example from '../database/models/ExampleModel';
 
-import { Response } from 'superagent';
-
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
 describe('Teams Api', () => {
-  /**
-   * Exemplo do uso de stubs com tipos
-   */
-
-  // let chaiHttpResponse: Response;
-
-  // before(async () => {
-  //   sinon
-  //     .stub(Example, "findOne")
-  //     .resolves({
-  //       ...<Seu mock>
-  //     } as Example);
-  // });
-
-  // after(()=>{
-  //   (Example.findOne as sinon.SinonStub).restore();
-  // })
-
-  // it('...', async () => {
-  //   chaiHttpResponse = await chai
-  //      .request(app)
-  //      ...
-
-  //   expect(...)
-  // });
-
   it('Recupera um time por id', async () => {
     const response = await chai.request(app).get('/teams/1');
 
@@ -54,5 +26,17 @@ describe('Teams Api', () => {
 
     expect(response.status).to.equal(404);
     expect(response.body).to.deep.equal({ message: 'Team 999 not found' });
+  });
+
+  it('Retornar todos os times', async () => {
+    const response = await chai.request(app).get('/teams');
+
+    expect(response.status).to.equal(200);
+    expect(response.body).to.deep.equal([
+      {
+        id: 1,
+        teamName: 'Example Team',
+      },
+    ]);
   });
 });
---
+++ app/backend/src/tests/maches.test.ts
@@ -0,0 +1,35 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import Example from '../database/models/ExampleModel';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Matches', () => {
+  it('Recupera todas as partidas', async () => {
+    const response = await chai.request(app).get('/matches');
+
+    expect(response.status).to.equal(200);
+  });
+
+  it('Recupera todas as partidas em andamento', async () => {
+    const response1 = await chai.request(app).post('/login').send({
+      email: 'admin@admin.com',
+      password: 'secret_admin',
+    });
+
+    const response = await chai.request(app).get('/matches/1/finish').set('Authorization', response1.body.token)
+    .send({
+      homeTeamGoals: 1,
+      awayTeamGoals: 2,
+    });
+
+    expect(response.status).to.equal(404);
+    expect(response.body).to.be.an('object');
+  });
+});
